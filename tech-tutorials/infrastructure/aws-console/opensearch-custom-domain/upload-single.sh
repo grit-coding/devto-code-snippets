8d890fd5a4bf5753b74b9811418b7a466ef4cf31
grit-coding <125764176+grit-coding@users.noreply.github.com>
2025-06-21 07:02:54 +0100

    timestamp

--- tech-tutorials/infrastructure/aws-console/opensearch-custom-domain/upload-single.sh
+++ tech-tutorials/infrastructure/aws-console/opensearch-custom-domain/upload-single.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 INDEX_NAME="my-index"
-AUTH="masterUserName:password" #change this to your username and password
-ENDPOINT="opensearchDomain/${INDEX_NAME}/_doc" #change this to your openSearch domain endpoint
+AUTH="gritcoding-admin:Gritcoding0*"
+ENDPOINT="https://search-demo.gritcoding.co.uk/${INDEX_NAME}/_doc"
 FILE="data.json"
 
 curl -u "$AUTH" -X POST "$ENDPOINT" \
