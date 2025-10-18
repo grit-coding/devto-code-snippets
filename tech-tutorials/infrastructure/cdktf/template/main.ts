5a10d8b46c7c71b1d286d8d7c991ab8128fb24da
grit-coding <n.gritcoding@gmail.com>
2025-10-18 12:41:01 +0100

    cdktf template example

--- tech-tutorials/infrastructure/cdktf/template/main.ts
+++ tech-tutorials/infrastructure/cdktf/template/main.ts
@@ -1,14 +1,22 @@
-import { Construct } from 'constructs';
-import { App, TerraformStack } from 'cdktf';
-
-class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-
-    // define resources here
-  }
-}
+import { App } from 'cdktf';
+import { EcrStack } from './stacks/ecr-stack';
 
 const app = new App();
-new MyStack(app, 'template');
+const keyPrefix = 'your-prefix';
+
+const sharedConfig = {
+  s3StateBucket: 'your-bucket-name' as const, // bucket and dynamodb are created ahead in each aws account
+  profile: 'grit-coding-terraform',
+  env: 'demo',
+  region: 'eu-west-2' as const,
+  projectPrefix: 'demo-infra',
+};
+
+//cdktf apply ecr-demo-stack
+new EcrStack(app, 'ecr-demo-stack', {
+  s3ObjectKey: `${keyPrefix}/ecr-stack.tfstate`,
+  ecrName: 'gritcoding-demo-ecr',
+  ...sharedConfig,
+});
+
 app.synth();
